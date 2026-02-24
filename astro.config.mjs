abcc12ca8744839553595a21cf47b06cefa9f923
fdeyuyo <fdeyuyo@gmail.com>
2026-02-24 15:51:55 -0300

    Config github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://finahuel.github.io',
+  base: '/Gambaastro/',
+  output: 'static'
+
+  
 });
